f6d6daa85e1cc2f372ec1867f93a9caaa3ace46b
Marcin <mazuromarcin@gmail.com>
2025-10-14 10:54:08 +0200

    add password change

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.logout, name='logout'),
     path('register/', views.register, name='register'),
+    path('password_change/', views.CustomPasswordChangeView.as_view(), name='password_change'),
     path('<str:username>/', views.profile, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import PasswordChangeView, LoginView
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, logout as auth_logout, login
-from .forms import LoginForm, CustomUserCreationForm
+from .forms import CustomUserCreationForm
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
 
 
 def profile(request, username):
@@ -12,24 +14,33 @@ def profile(request, username):
     return render(request, 'accounts/profile.html', {'username': username})
 
 
-def user_login(request):
-    if request.user.is_authenticated:
-        return redirect('core:index')
+# def user_login(request):
+#     if request.user.is_authenticated:
+#         return redirect('core:index')
     
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('accounts:profile', username=user.username)
-            else:
-                form.add_error(None, 'Invalid username or password')
-    else:
-        form = LoginForm()
-    return render(request, 'accounts/login.html', {'form': form})
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password']
+#             user = authenticate(request, username=username, password=password)
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect('accounts:profile', username=user.username)
+#             else:
+#                 form.add_error(None, 'Invalid username or password')
+#     else:
+#         form = LoginForm()
+#     return render(request, 'accounts/login.html', {'form': form})
+
+
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('accounts:profile', kwargs={'username': self.request.user.username})
+
 
 
 def logout(request):
@@ -50,3 +61,10 @@ def register(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
+
+
+class CustomPasswordChangeView(PasswordChangeView):
+    template_name = 'accounts/password_change.html'
+
+    def get_success_url(self):
+        return reverse_lazy('accounts:profile', kwargs={'username': self.request.user.username})  
